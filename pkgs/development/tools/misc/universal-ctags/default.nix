dd767e82ed129e43aedebac8d51cfdd79bef6700
Ivan Babrou <github@ivan.computer>
2021-01-31 17:11:03 -0800

    universal-ctags: downgrade to p5.9.20201206.0
    Newer versions fail on aarch64-linux:
    
    ```
      checking for aarch64-unknown-linux-gnu-gcc... aarch64-unknown-linux-gnu-gcc
      checking whether the compiler supports GNU C... yes
      checking whether aarch64-unknown-linux-gnu-gcc accepts -g... yes
      checking for aarch64-unknown-linux-gnu-gcc option to enable C11 features... (cached) none needed
      checking whether aarch64-unknown-linux-gnu-gcc understands -c and -o together... yes
      checking dependency style of aarch64-unknown-linux-gnu-gcc... none
      checking whether the C compiler works... no
      configure: error: in `/build/source':
      configure: error: C compiler cannot create executables
      See `config.log' for more details
    ```
    
    The following PR is likely culprit:
    
    * https://github.com/universal-ctags/ctags/pull/2747

--- pkgs/development/tools/misc/universal-ctags/default.nix
+++ pkgs/development/tools/misc/universal-ctags/default.nix
@@ -2,13 +2,13 @@
 
 stdenv.mkDerivation rec {
   pname = "universal-ctags";
-  version = "5.9.20210124.0";
+  version = "5.9.20201206.0";
 
   src = fetchFromGitHub {
     owner = "universal-ctags";
     repo = "ctags";
     rev = "p${version}";
-    sha256 = "082yq629s2p4fc5kmjhmzx9q9grc32hwfr1x55ydvhi4x68dbx6d";
+    sha256 = "0w10zjyz46sjm6ypxmq550dkr84hvc4phm4vm9j53jp5s19x5q19";
   };
 
   nativeBuildInputs = [ autoreconfHook pkg-config python3Packages.docutils ];
